ae0d203e75ff4b9ae9228b999aa00a26fe7e536b
indextrown <lghan00020@nate.com>
2024-07-17 00:29:54 +0900

    update

--- iOS/ChairFriend/ChairFriend/WelcomeViewController.swift
+++ iOS/ChairFriend/ChairFriend/WelcomeViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var signUpButton: UIButton!
     
@@ -23,6 +23,17 @@ class WelcomeViewController: UIViewController {
         
         self.signUpButton.layer.masksToBounds = true
         self.signUpButton.layer.cornerRadius = 30
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        print("Login button tapped")
+        let storyboard = UIStoryboard(name: "Main", bundle: nil) // Main.storyboard 가져오기
+        guard let loginVC = storyboard.instantiateViewController(withIdentifier: "LoginView") as? LoginViewController else {
+            print("LoginViewController could not be instantiated.")
+            return
+        }
+        loginVC.modalPresentationStyle = .fullScreen
+        self.present(loginVC, animated: true, completion: nil)
     }
 }
 
