cb56198994c24b6bd330bbee8f434c81aa339e21
jh00-j <wjs3048@naver.com>
2024-07-17 17:39:33 +0900

    change

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -1,12 +1,21 @@
+import java.io.FileInputStream
+import java.util.Properties
+import kotlin.script.experimental.api.ScriptCompilationConfiguration.Default.properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     
 }
-
+val properties = Properties()
+properties.load(project.rootProject.file("local.properties").inputStream())
 android {
     namespace = "com.example.chairfriend"
     compileSdk = 34
+
+    buildFeatures {
+        buildConfig = true
+    }
 
     defaultConfig {
         applicationId = "com.example.chairfriend"
@@ -16,6 +25,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "api_key", properties["api_key"] as String)
+        manifestPlaceholders["api_key"] = properties["api_key"] as Any
     }
 
     buildTypes {
@@ -43,6 +55,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.play.services.maps)
+
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ Android/app/src/main/java/com/example/chairfriend/Main4Activity.kt
@@ -0,0 +1,46 @@
+package com.example.chairfriend
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class Main4Activity : AppCompatActivity(), OnMapReadyCallback {
+
+    private var mMap: GoogleMap? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main4)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.mapView2) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val SEOUL = LatLng(37.556, 126.97)
+
+        val markerOptions = MarkerOptions()
+        markerOptions.position(SEOUL)
+        markerOptions.title("서울")
+        markerOptions.snippet("한국 수도")
+
+        mMap?.addMarker(markerOptions)
+        mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10f))
+    }
+}
